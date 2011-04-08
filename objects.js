e45b3f21e6871098c297ab23015c18272bc412f7
Matthew Wastrodowski <towski@gmail.com>
2011-04-08 14:49:35 -0700

    add some tests and basic talking functionality

--- objects.js
+++ objects.js
@@ -1,9 +1,12 @@
+sys = require('sys')
+
 Player = function(config){  
   this.initialize(config);
 }
 
 Player.prototype = {
   initialize: function(config){
+    this.attractiveness = 2;
     this.esteem = 50;
     this.lines;
   },
@@ -11,6 +14,10 @@ Player.prototype = {
     var response = girl.respondToGlance(this);
     this.esteem += response['esteem'];
     return response.line;
+  },
+  talk: function(girl, line){
+    var response = girl.talk(this, line);
+    return response.line
   }
 }
 
@@ -20,10 +27,56 @@ Girl = function(config){
 
 Girl.prototype = {
   initialize: function(config){
-    this.esteem = config;
+    this.esteem = config.esteem || 50;
+    this.attractiveness = config.attractiveness || 5;
     this.lines;
   },
   respondToGlance: function(player){
     return {line: "She doesn't seem to notice", esteem: -1}
+  },
+  talk: function(player, line){
+    if(line.greeting){
+      if(player.attractiveness > this.attractiveness && this.chance(0.5)){
+        return {line: "Hi", esteem: 1};
+      }else{
+        return {line: "...", esteem: -1};
+      }
+    }else if(line.question) {
+      if(player.attractiveness + 2 > this.attractiveness && this.chance(0.5)){
+        this.comfortable = true;
+        return {line: line.success, esteem: 1};
+      }else{
+        return {line: line.failure, esteem: -1};
+      }
+    }else if(line.ask_out) {
+      if(player.attractiveness > this.attractiveness + 2 && this.comfortable && this.chance(0.25)){
+        return {line: line.success, esteem: 1};
+      }else{
+        return {line: line.failure, esteem: -1};
+      }
+    }
+  },
+  chance: function(probability){
+    var chance = Math.random();
+    if(chance > probability){
+      return true;
+    }else{
+      return false
+    }
+  }
+}
+
+Line = function(config){
+  this.initialize(config);
+}
+
+Line.prototype = {
+  initialize: function(config){
+    this.line = config.line;
+    this.greeting = config.greeting;
+    this.question = config.question;
+    this.success = config.success;
+    this.failure = config.failure;
+    this.ask_out = config.ask_out;
   }
 }
--- test.js
+++ test.js
@@ -1,5 +1,42 @@
 var assert = require('assert');
+var sys = require('sys');
+var nodeunit = require('nodeunit');
 require('./objects.js')
-var player = new Player()
-var girl = new Girl();
-assert.equal("She doesn't seem to notice", player.glance(girl));
+
+exports['test1'] = nodeunit.testCase({
+  'test1': function(test){
+    player = new Player()
+    girl = new Girl({attractiveness: 5});
+    girl.chance = function(){ return true };
+    assert.equal("She doesn't seem to notice", player.glance(girl));
+    var line = new Line({line: "Hey", greeting: true});
+    assert.equal("...", player.talk(girl, line));
+    player.attractiveness = 6
+    assert.equal("Hi", player.talk(girl, line));
+    test.done();
+  },
+
+  'test2': function(test){
+    player = new Player()
+    girl = new Girl({attractiveness: 5});
+    girl.chance = function(){ return true };
+    var line2 = new Line({line: "Do you know where a burger place is?", question:true, success: "I think there's one up the street", failure: "Nope, sorry"});
+    player.attractiveness = 1
+    assert.equal("Nope, sorry", player.talk(girl, line2));
+    player.attractiveness = 4
+    assert.equal("I think there's one up the street", player.talk(girl, line2));
+    test.done();
+  },
+
+  'test3': function(test){
+    player = new Player()
+    girl = new Girl({attractiveness: 5});
+    girl.chance = function(){ return true };
+    var line3 = new Line({line: "Wanna grab a burger?", ask_out:true, success: "Sure", failure: "...? No thanks"});
+    player.attractiveness = 1
+    assert.equal(line3.failure, player.talk(girl, line3));
+    player.attractiveness = 7
+    assert.equal(line3.failure, player.talk(girl, line3));
+    test.done();
+  }
+});
